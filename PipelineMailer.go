9bef43248ff77a5edbe18b32d1565d8cae2a91a6
ScorchingShade <ankushors789@gmail.com>
2018-07-04 12:35:44 +0530

    Creating Plugin for Kapacitor push1

--- PipelineMailer.go
+++ PipelineMailer.go
@@ -1,1 +1,40 @@
 package PluginOSS
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+
+	"github.com/influxdata/kapacitor/tick/ast"
+	"github.com/pkg/errors"
+
+type AlertNode struct{ *AlertNodeData }
+
+type AlertNodeData struct {
+
+	Category string `json:"category"`
+	Topic string `json:"topic"`
+	}
+
+
+
+
+func (n *AlertNodeData) Email(to ...string) *EmailHandler {
+	em := &EmailHandler{
+		AlertNodeData: n,
+		ToList:        to,
+	}
+	n.EmailHandlers = append(n.EmailHandlers, em)
+	return em
+}
+
+// Email AlertHandler
+// tick:embedded:AlertNode.Email
+type EmailHandler struct {
+	*AlertNodeData `json:"-"`
+
+	// List of email recipients.
+	// tick:ignore
+	ToList []string `tick:"To" json:"to"`
+}
